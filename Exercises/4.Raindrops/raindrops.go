6b9efecea977507d756fdd4a65aa3074340f4ef1
ericcchiu <eric.chn.chiu@gmail.com>
2021-04-05 15:05:08 -0500

    (FEATURE) Add Raindrops solution

--- Exercises/4.Raindrops/raindrops.go
+++ Exercises/4.Raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
-func Convert(num int64) string {
-	// Your code here
+import "strconv"
+
+// Convert function that takes an integer and returns Pling, Plang, Plong if number are divisble by 3,5,7 and returns stringified number if not.
+func Convert(num int) (output string) {
+
+	if num%3 == 0 {
+		output += "Pling"
+	}
+
+	if num%5 == 0 {
+		output += "Plang"
+	}
+
+	if num%7 == 0 {
+		output += "Plong"
+	}
+
+	if output == "" {
+		output = strconv.Itoa(num)
+	}
+
+	return
 }
